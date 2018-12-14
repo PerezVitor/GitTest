942c2f69e305a139f0e9a42ebde5738a7f02d8b4
Vitor Perez <vitor.g.perez@hotmail.com>
2018-12-14 17:43:44 -0200

    Criando teste de Git

---
+++ Teste_Git_Github/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Teste_Git_Github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Para este projeto veremos o funcionamento do Git");
+
+            Console.ReadLine();
+        }
+    }
+}
